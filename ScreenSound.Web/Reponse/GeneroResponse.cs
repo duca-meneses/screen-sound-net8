f81163da3061a78c18967cf38a310e24ef511572
Carlos Eduardo <ducameneses16@gmail.com>
2024-02-26 20:44:14 -0300

    feat: Iniciando aula 4 preparando ambiente

--- ScreenSound.Web/Reponse/GeneroResponse.cs
+++ ScreenSound.Web/Reponse/GeneroResponse.cs
@@ -1,3 +1,9 @@
 ﻿namespace ScreenSound.Web.Reponse;
 
-public record GeneroResponse(int Id, string Nome, string Descricao);
+public record GeneroResponse(int Id, string Nome, string Descricao)
+{
+    public override string ToString()
+    {
+        return $"{this.Nome}";
+    }
+}
