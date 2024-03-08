5ad1bffcb786531a5acc2ae9fe6eb8d6acacdcb3
Carlos Eduardo <ducameneses16@gmail.com>
2024-03-08 20:32:30 -0300

    feat: Added the auth service and created the auth response

---
+++ ScreenSound.Web/Reponse/AuthResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace ScreenSound.Web.Reponse;
+
+public class AuthResponse
+{
+    public bool Sucesso { get; set; }
+    public string[] Erros {  get; set; }
+}
---
+++ ScreenSound.Web/Services/AuthAPI.cs
@@ -0,0 +1,24 @@
+﻿using ScreenSound.Web.Reponse;
+using System.Net.Http.Json;
+
+namespace ScreenSound.Web.Services;
+
+public class AuthAPI(IHttpClientFactory factory)
+{
+    private readonly HttpClient _httpClient = factory.CreateClient("API");
+
+    public async Task<AuthResponse> LoginAsync(string email, string senha)
+    {
+        var response = await _httpClient.PostAsJsonAsync("auth/login", new
+        {
+            email,
+            password = senha
+        });
+
+        if (response.IsSuccessStatusCode)
+        {
+            return new AuthResponse { Sucesso = true };
+        }
+        return new AuthResponse { Sucesso = false, Erros = ["Login/senha iválidos"] };
+    }
+}
