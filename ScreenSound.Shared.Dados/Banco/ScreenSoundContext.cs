d3e6561d2e34ac5b339d7ad3c89199612d8298ec
Carlos Eduardo <ducameneses16@gmail.com>
2024-03-12 02:21:06 -0300

    feat: Adding the dbset for AvaliacaoArtista and generating migrations with Entity Framework

--- ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
+++ ScreenSound.Shared.Dados/Banco/ScreenSoundContext.cs
@@ -9,6 +9,7 @@ public class ScreenSoundContext : IdentityDbContext<PessoaComAcesso, PerfilDeAce
     public DbSet<Artista> Artistas { get; set; }
     public DbSet<Musica> Musicas { get; set; }
     public DbSet<Genero> Generos { get; set; }
+    public DbSet<AvaliacaoArtista> AvaliacaoArtistas { get; set; }  
 
 
     private string dbconnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSoundV0;Integrated Security=True;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
@@ -38,6 +39,9 @@ public class ScreenSoundContext : IdentityDbContext<PessoaComAcesso, PerfilDeAce
         modelBuilder.Entity<Musica>()
             .HasMany(c => c.Generos)
             .WithMany(C => C.Musicas);
+
+        modelBuilder.Entity<AvaliacaoArtista>()
+            .HasKey(a => new { a.ArtistaId, a.PessoaId });
 
     }
 }
--- ScreenSound.Shared.Dados/Migrations/20240312033638_AvaliacaoArtistas.Designer.cs
+++ ScreenSound.Shared.Dados/Migrations/20240312033638_AvaliacaoArtistas.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ScreenSound.Shared.Dados.Banco;
 
@@ -11,9 +12,11 @@ using ScreenSound.Shared.Dados.Banco;
 namespace ScreenSound.Migrations
 {
     [DbContext(typeof(ScreenSoundContext))]
-    partial class ScreenSoundContextModelSnapshot : ModelSnapshot
+    [Migration("20240312033638_AvaliacaoArtistas")]
+    partial class AvaliacaoArtistas
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
